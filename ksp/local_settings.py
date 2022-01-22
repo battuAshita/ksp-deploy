b8987252fa12203e5e2d11accce0d11e2471bcf4
battuAshita <battua@iitbhilai.ac.in>
2022-01-22 12:19:35 +0530

    deploy to heroku

---
+++ ksp/local_settings.py
@@ -0,0 +1,11 @@
+#local_settings.py :
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'd2341abehcrhhf',                      
+        'USER': 'ielwglqegfynsk',                     
+        'PASSWORD': '7b7d535b7c4415b839bea44056e0b0666b133f6eb33440004f42e5e44d02fd7f',                  
+        'HOST': 'ec2-35-168-80-116.compute-1.amazonaws.com',                      
+        'PORT': '5432',                      
+    }
+}
